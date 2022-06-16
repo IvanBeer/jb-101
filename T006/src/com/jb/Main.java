236af642fbd73e1d2d1310a16aea510b5075cbe5
IvanBeer <mahortov_play@mail.ru>
2022-06-17 00:01:01 +0300

    T006

---
+++ T006/src/com/jb/Main.java
@@ -0,0 +1,12 @@
+package com.jb;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int g = 1;
+        for(int x = 1; x != 10; x++) {
+            g = g * 2;
+            System.out.print(g + " ");
+        }
+    }
+}
