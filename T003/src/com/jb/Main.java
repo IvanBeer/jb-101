0f7e04588b915a9c1c7f319af11a465fb0ece62f
IvanBeer <mahortov_play@mail.ru>
2022-06-16 23:21:57 +0300

    T003

---
+++ T003/src/com/jb/Main.java
@@ -0,0 +1,11 @@
+package com.jb;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int y = 3;
+	    for( int x = 1; x != 11; x++){
+            System.out.println(y + " * " + x + " = " + (x*y));
+        }
+    }
+}
